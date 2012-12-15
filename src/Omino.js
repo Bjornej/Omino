df81ff002554d65fbf42e77f3a548affcfd185b3
Paolo Nicodemo <paolonicodemo@gmail.com>
2012-12-15 21:20:05 +0100

    added extend method

--- src/Omino.js
+++ src/Omino.js
@@ -1,1 +1,28 @@
 var Omino = {};
+
+var extend = function(protoProps, staticProps) {
+		var parent = this;
+		var child;
+
+		if(protoProps && _.has(protoProps, 'constructor')) {
+			child = protoProps.constructor;
+		} else {
+			child = function() {
+				parent.apply(this, arguments);
+			};
+		}
+
+		_.extend(child, parent, staticProps);
+
+		var Surrogate = function() {
+				this.constructor = child;
+			};
+		Surrogate.prototype = parent.prototype;
+		child.prototype = new Surrogate;
+
+		if(protoProps) _.extend(child.prototype, protoProps);
+
+		child.base = parent.prototype;
+
+		return child;
+	};
