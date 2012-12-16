96ac6444d8bc7ee194b06597e93eda6454e193f1
Paolo Nicodemo <paolonicodemo@gmail.com>
2012-12-16 15:59:51 +0100

    created initial View implementation

--- src/Omino.View.js
+++ src/Omino.View.js
@@ -1,7 +1,26 @@
 var View = Omino.View = function(){
-
-
+	var el = "<" + this.tagName;
+	if(this.className){el+= " class=\"" + this.className + "\"";}
+	el += " />";
+	this.el = el;
+	this.$el = $(el);
 };
 
+_.extend(View.prototype, Omino.Events, {
+
+	tagName : "div",
+
+	render : function(){
+		this.trigger("before:render");
+		this.$el.html(this.template(this.serializeData()));
+		this.trigger("after:render");
+	},
+
+	serializeData : function(){
+		var model = this.model.toJSON();
+		model.options = this.options;
+		return model;
+	}
+});
 
 View.extend = extend;
