193c33723924d2ffaf609c2a6980ee83a5ab25c6
Paolo Nicodemo <paolonicodemo@gmail.com>
2013-01-01 17:41:51 +0100

    added view tests

--- test/spec/Test.View.js
+++ test/spec/Test.View.js
@@ -1,29 +1,109 @@
-describe("View",function(){
-	var testView,testInstance;
+describe("View", function() {
+	var testView, testInstance;
 
-	beforeEach(function(){
+	beforeEach(function() {
 		testView = Omino.View.extend({});
 		testInstance = new testView({});
 	});
 
-	describe("constructor",function(){
-		it("creates a div tag",function(){
+	describe("constructor", function() {
+		it("creates a div tag", function() {
 			testInstance.el.should.be.equal("<div />");
 		});
 
 
-		it("creates a specific tag",function(){
-			testView = Omino.View.extend({ tagName : "table"});
+		it("creates a specific tag", function() {
+			testView = Omino.View.extend({
+				tagName: "table"
+			});
 			testInstance = new testView({});
 			testInstance.el.should.be.equal("<table />");
 		});
 
-		it("creates el with a specific class",function(){
-			testView = Omino.View.extend({ className : "prova"});
+		it("creates el with a specific class", function() {
+			testView = Omino.View.extend({
+				className: "prova"
+			});
 			testInstance = new testView({});
 			testInstance.el.should.be.equal("<div class=\"prova\" />");
 		});
 
 	});
+
+
+	describe("render", function() {
+		it("should call beforeRender",function(){
+			var test = sinon.spy();
+			testView = Omino.View.extend({
+				beforeRender : test,
+				template : function(){return "";},
+				serializeData:function(){return {};}
+			});
+			testInstance = new testView({});
+			testInstance.render();
+			test.should.have.been.called;
+		});
+
+		it("should trigger the before:render event",function(){
+			var test = sinon.spy();
+			testView = Omino.View.extend({
+				template : function(){return "";},
+				serializeData:function(){return {};}
+			});
+
+			testInstance = new testView({});
+			testInstance.on("before:render",test);
+			testInstance.render();
+			test.should.have.been.called;
+		});
+
+		it("should call afterRender",function(){
+			var test = sinon.spy();
+			testView = Omino.View.extend({
+				afterRender : test,
+				template : function(){return "";},
+				serializeData:function(){return {};}
+			});
+			testInstance = new testView({});
+			testInstance.render();
+			test.should.have.been.called;
+		});
+
+		it("should trigger the after:render event",function(){
+			var test = sinon.spy();
+			testView = Omino.View.extend({
+				template : function(){return "";},
+				serializeData:function(){return {};}
+			});
+
+			testInstance = new testView({});
+			testInstance.on("after:render",test);
+			testInstance.render();
+			test.should.have.been.called;
+		});
+
+		it("should render the template function set as template property",function(){
+			testView = Omino.View.extend({
+				template : function(){return "test"},
+				serializeData:function(){return {};}
+			});
+			testInstance = new testView({});
+			testInstance.render();
+			testInstance.$el.text().should.be.equal("test");
+		});	
+
+		it("should pass the serializeData function result to the function template",function(){
+			var test=sinon.spy();
+			testView = Omino.View.extend({
+				template : test,
+				serializeData:function(){return { prova : 2};}
+			});
+			testInstance = new testView({});
+			testInstance.render();
+			test.should.have.been.calledWith({ prova : 2});
+		});	
+
+	});
+
 
 });
